e295cbc213033510703701a1d0028318dbb15dc2
Shimon Aizenbach <s0533137261@gmail.com>
2023-08-16 09:58:23 +0300

    stage 2 I did it

--- server1.js
+++ server1.js
@@ -1,21 +1,28 @@
 const express = require(`express`);
 const morgan = require(`morgan`);
 const cors = require(`cors`);
-const { log } = require("console");
+const { v4: uuidv4 } = require('uuid');
+
+const myId = () => uuidv4();
+const allIdAscending = [
+   `8cec6301-bd35-48e7-b17d-5e0813a9ba5b`,
+   `688152d5-0a75-4df8-ba0b-2a50fad4d27b`,
+  `ff2def83-b3d7-4714-b533-c3c5cb7dd0cf`
+]
 
 const data = [
   {
-    id: 1,
+    id: allIdAscending[0],
     email: `123@123.123`,
     password: `12345`,
   },
   {
-    id: 2,
+    id: allIdAscending[1],
     email: `1234@123.123`,
     password: `54321`,
   },
   {
-    id: 3,
+    id: allIdAscending[2],
     email: `12345@123.123`,
     password: `2468`,
   },
@@ -29,7 +36,7 @@ app.use(cors());
 app.use(morgan(`tiny`));
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send(
+  res.send( 
     data.map(
       (user) =>
         `id: ${user.id}, email: ${user.email}, password: ${user.password}`
@@ -38,31 +45,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:id", (req, res) => {
-  const user = data.filter((user) => user.id === Number(req.params.id))[0];
+  const user = data.filter((user) => user.id === req.params.id)[0];
   res.send(`id: ${user.id}, email: ${user.email}, password: ${user.password}`);
 });
 
 app.post("/", (req, res) => {
+  allIdAscending.push(myId())
+  console.log(allIdAscending)
   data.push({
-    id: data.length + 1,
+    id: allIdAscending[data.length],
     email: req.body.email,
     password: req.body.password,
   });
-  console.log(data[data.length - 1]);
+  console.log(...data);
   res.send(`User created`);
 });
 
-app.put("/:id", (req, res) => { 
+app.put("/:id", (req, res) => {
   data[req.params.id - 1].email = req.body.email;
   data[req.params.id - 1].password = req.body.password;
   console.log(data[req.params.id - 1]);
   res.send(`User has been edited`);
 });
 
-app.delete("/:id", (req, res) => { 
-    data[req.params.id - 1] = {};
-    console.log(data[req.params.id - 1]);
-    res.send(`User has been deleted`);
-  });
-
-
+app.delete("/:id", (req, res) => {
+  data[req.params.id - 1] = {};
+  console.log(data[req.params.id - 1]);
+  res.send(`User has been deleted`);
+});
